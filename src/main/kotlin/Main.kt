405d6e7c8a5d8fc6e578e1eb1870d8cb4f7744f6
archykins <ulliay-1557@yandex.ru>
2022-11-26 21:18:27 +0300

    mess

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
-
+//Вывести только те слова сообщения, которые содержат не более чем n
+//букв
+    print("Введите сообщение - ")
+    val text: String = readln()
+    print("Введите количество букв - ")
+    var n = readln()
+    val lines: List<String> = text.lines()
+    println(text.count())
+    // Не хватает ума дописать, смотрю получится ли отправить в хаб
 }
