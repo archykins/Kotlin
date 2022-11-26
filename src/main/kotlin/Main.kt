c9f23f3e825e9cf83ae31921ae51e6987238be61
archykins <ulliay-1557@yandex.ru>
2022-11-26 20:35:56 +0300

    mess

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,3 @@
 fun main(args: Array<String>) {
-    println("Разработать программу, которая для заданной строки s подсчитывает количество букв в строке")
-    print("Введите текст строки - ")
-    val stringStroka: String = (readln())
-    val pop = stringStroka.replace("[^a-zA-Zа-яёА-ЯЁ]".toRegex(), "")
-    print("Число букв в строке - ")
-    println(pop.count())
+
 }
